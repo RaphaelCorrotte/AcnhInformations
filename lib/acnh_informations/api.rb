8c26d3f3df2d34b64bea45e8cfc61171cc80b8b5
senchuu <senchuuuu@gmail.com>
2021-06-08 19:37:30 +0200

    Update the api class

--- lib/acnh_informations/api.rb
+++ lib/acnh_informations/api.rb
@@ -3,7 +3,16 @@
 require "rest-client"
 
 module AcnhInformations
+  # Scrape the informations from the 'acnhapi' api
   class Api
-    BASE_URL = "http://acnhapi.com/"
+    BASE_URL = "http://acnhapi.com"
+    CATEGORIES = %w[fish sea bugs fossils villagers icons images songs music art houseware].freeze
+
+    def self.valid?(category, id)
+      RestClient.get("#{BASE_URL}/v1/#{category.to_s}/#{id.to_s}")
+      true
+    rescue RestClient::NotFound
+      false
+    end
   end
 end
