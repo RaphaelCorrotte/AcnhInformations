1dfec3c60b5c6a4048388ce7997875386c7e4073
senchuu <senchuuuu@gmail.com>
2021-06-09 18:56:41 +0200

    Create scrape class method and add documentation

--- lib/acnh_informations/api.rb
+++ lib/acnh_informations/api.rb
@@ -1,18 +1,40 @@
 # frozen_string_literal: true
 
 require "rest-client"
+require "json"
 
 module AcnhInformations
-  # Scrape the informations from the 'acnhapi' api
+  # Scrape informations from 'acnhapi' api
   class Api
-    BASE_URL = "http://acnhapi.com"
+    # The base URL to scrape
+    BASE_URL = "https://acnhapi.com"
+    # All available categories
     CATEGORIES = %w[fish sea bugs fossils villagers icons images songs music art houseware].freeze
 
-    def self.valid?(category, id)
-      RestClient.get("#{BASE_URL}/v1/#{category.to_s}/#{id.to_s}")
+    ##
+    # Verify if the object exists
+    #
+    # @param [Symbol, StringIO] category The category to find the object
+    # @param id [Symbol, StringIO, NilClass] The id to get
+    # @return [Boolean] True or False, if the wanted research exists
+    def self.valid?(category, id = nil)
+      RestClient.get("#{BASE_URL}/v1/#{category.to_s}/#{id ? id.to_s : ""}")
       true
     rescue RestClient::NotFound
       false
+    end
+
+    ##
+    # Scrape informations from API
+    #
+    # @param [Symbol, StringIO] category The category to find the object
+    # @param id [Symbol, StringIO, NilClass] The id to get
+    # @return [Object] The result as a Hash
+    # @see Api#valid?
+    def self.scrape(category, id = nil)
+      return false unless valid?(category, id)
+
+      JSON.parse(RestClient.get("#{BASE_URL}/v1/#{category.to_s}/#{id ? id.to_s : ""}"), :symbolize_names => true)
     end
   end
 end
