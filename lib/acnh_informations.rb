5188eb9c7b406c669ac9ab1d870146cec4a9b460
senchuu <senchuuuu@gmail.com>
2021-06-13 18:50:15 +0200

    small changes

--- lib/acnh_informations.rb
+++ lib/acnh_informations.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 # frozen_string_literal: true
 
 require_relative "acnh_informations/version"
@@ -7,5 +9,3 @@ require_relative "acnh_informations/tools"
 # The main module
 module AcnhInformations
 end
-
-p AcnhInformations::Api.get_by_name(:fish, "bouv")
